fc31dd030d04815f47f2d7e305ee8f1953aa085c
jabberwock <dark.jabberwock+github@gmail.com>
2019-01-10 20:09:58 +0100

    add hltb command

---
+++ commands/hltb.js
@@ -0,0 +1,6 @@
+module.exports.command  = 'hltb'
+module.exports.help     = 'HowLongToBeat'
+module.exports.callback = (message, words) => {
+    
+    message.reply("Le site est là : https://howlongtobeat.com/ \n Débrouille toi !");
+};
