249f28baf0d6c1c0cef8b764a38271b6e9c4c6f8
louckousse <louckousse@gmail.com>
2019-01-12 12:08:03 +0100

    Prendre en compte les retours d'Yvonnick

--- commands/hltb.js
+++ commands/hltb.js
@@ -3,21 +3,23 @@ let hltbService = new hltb.HowLongToBeatService();
 
 module.exports.command = 'hltb'
 module.exports.help = 'HowLongToBeat'
-module.exports.callback = (message, words) => {
-    const getRes = (stg) => {
-        if (stg.length < 1) return "oh no, j'ai rien trouvé ☹"
-        const game = stg[0]
-        let res = game.name
-        const mainStory = game.gameplayMain
-        const completionist = game.gameplayCompletionist
-        if (mainStory || completionist) res += " se finit en "
-        else return "Pas d'info pour " + res
-        if (mainStory) res += mainStory + "h si tu traces" + (completionist ? ", " : ".")
-        if (completionist) res += completionist + "h si tu veux tout faire."
-        return res;
-    }
+module.exports.callback = async (message, words) => {
     if (words.length > 1) {
-        hltbService.search(words.slice(1).join(' ')).then(result => message.reply(getRes(result)))
+        const result = await hltbService.search(words.slice(1).join(' '))
+        if (result.length == 0) message.reply("oh no, j'ai rien trouvé ☹")
+        else {
+            game = result[0]
+            const {
+                gameplayMain: mainStory,
+                gameplayCompletionist: completionist,
+            } = game
+            let res = game.name
+            if (mainStory || completionist) res += " se finit en "
+            else return "Pas d'info pour " + res
+            if (mainStory) res += mainStory + "h si tu traces" + (completionist ? ", " : ".")
+            if (completionist) res += completionist + "h si tu veux tout faire."
+            message.reply(res)
+        }
     } else {
         message.reply('Tu cherches quoi ?');
     }
